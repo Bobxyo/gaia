3678e5871a5befca4d0503666ec5d1519c7134e9
Bobxyo <bobxyo@gmail.com>
2023-02-13 17:52:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,45 @@
-import os
 import streamlit as st
-import openai
+import requests
+import os
 
-# Hide API Key
-API_KEY = os.getenv("KEY")
-openai.api_key = API_KEY
+KEY = os.environ.get("KEY", None)
 
-# Set up chat history
-history = []
+st.set_page_config(page_title="Chat with OpenAI GPT3", page_icon=":robot:", layout="wide")
 
-def chat(model, prompt):
-    completions = openai.Completion.create(
-        engine=model,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+def chat_with_openai(prompt):
+    model_engine = "text-davinci-003"
+    prompt = (f"User: {prompt}" + "\n")
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': f'Bearer {KEY}'
+    }
 
-    message = completions.choices[0].text
-    history.append(f"Me: {prompt}")
-    history.append(f"ChatGPT: {message}")
+    response = requests.post(
+      f"https://api.openai.com/v1/engines/{model_engine}/jobs",
+      headers=headers,
+      json={
+        "prompt": prompt,
+        "max_tokens": 100,
+        "temperature": 0.5,
+      }
+    ).json()
+
+    message = response['choices'][0]['text']
     return message
 
-# Create the Streamlit app
-st.set_page_config(page_title="ChatGPT", page_icon=":robot:", layout="wide")
-st.title("ChatGPT")
+def main():
+    st.title("Chat with OpenAI's GPT-3")
+    history = []
+    while True:
+        user_input = st.text_input("You:")
+        if user_input == "exit":
+            break
+        if user_input:
+            response = chat_with_openai(user_input)
+            history.append(f"User: {user_input}")
+            history.append(f"GPT-3: {response}")
+            st.write("GPT-3:", response)
+    st.write("History of this session:", history)
 
-model = "text-davinci-003"
-prompt = st.text_input("Enter your message:")
-
-if st.button("Submit"):
-    response = chat(model, prompt)
-    st.write("ChatGPT: ", response)
-
-# Show chat history
-if len(history) > 0:
-    st.header("Chat History:")
-    for h in history:
-        st.write(h)
+if __name__ == "__main__":
+    main()
